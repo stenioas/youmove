ede267182876cb87c8757754827d271e9a749b4e
stenioas <stenioas@gmail.com>
2021-02-26 21:22:59 -0300

    Diminuindo o tempo para testes

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -37,7 +37,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     clearTimeout(countdownTimeout);
     setIsActive(false);
     setHasFinished(false);
-    setTime(25 * 60);
+    setTime(3);
   }
 
   useEffect(() => {
